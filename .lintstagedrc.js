43846781692f0892da77c17a9560aa265aac2f0c
Thiago Kachinsky <thiagokachidev@gmail.com>
2024-03-15 20:43:31 -0300

    chore: config plop and ci

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -3,7 +3,7 @@ module.exports = {
   '*.{js,jsx,ts,tsx}': (filenames) => [
     `prettier --write ${filenames.join(' ')}`,
     `npm run lint --fix . ${filenames.join(' --file')}`,
-    `npm test -- --findRelatedTests ${filenames.join(' ')}`,
+    `npm test -- --findRelatedTests --passWithNoTests ${filenames.join(' ')}`,
   ],
   // '*.spec.tsx': (filenames) => [],
 };
