10487ca489fee460c7c67019ae5f94cfa597abec
Thiago Kachinsky <thiagokachidev@gmail.com>
2024-04-01 20:34:01 -0300

    feat: create button component

---
+++ src/components/Button/Button.stories.tsx
@@ -0,0 +1,30 @@
+import { Meta, StoryFn } from '@storybook/react';
+import { AddShoppingCart } from '@styled-icons/material-outlined/AddShoppingCart';
+import { Button } from '.';
+
+export default {
+  title: 'Button',
+  component: Button,
+  argTypes: {
+    children: {
+      type: 'string',
+    },
+    icon: {
+      type: 'symbol',
+    },
+  },
+} as Meta;
+
+export const Default: StoryFn = (args) => <Button {...args} />;
+
+Default.args = {
+  children: 'Buy now',
+};
+
+export const withIcon: StoryFn = (args) => <Button {...args} />;
+
+withIcon.args = {
+  size: 'small',
+  children: 'Buy now',
+  icon: <AddShoppingCart />,
+};
