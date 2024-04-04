299d21a8d0bc3f84f2d609e4ee2b213b890b6dd5
Thiago Kachinsky <thiagokachidev@gmail.com>
2024-04-03 21:14:04 -0300

    feat: create footer and some adjusts in existing components

--- src/components/Heading/index.tsx
+++ src/components/Heading/index.tsx
@@ -1,10 +1,14 @@
 import * as S from './styles';
 
+export type LineColors = 'primary' | 'secondary';
+
 export type HeadingProps = {
   children: React.ReactNode;
   color?: 'white' | 'black';
   $lineLeft?: boolean;
   $lineBottom?: boolean;
+  $lineColor?: LineColors;
+  size?: 'small' | 'medium';
 };
 
 export function Heading({
@@ -12,9 +16,17 @@ export function Heading({
   color = 'white',
   $lineLeft = false,
   $lineBottom = false,
+  $lineColor = 'primary',
+  size = 'medium',
 }: HeadingProps) {
   return (
-    <S.Wrapper color={color} $lineLeft={$lineLeft} $lineBottom={$lineBottom}>
+    <S.Wrapper
+      color={color}
+      $lineLeft={$lineLeft}
+      $lineBottom={$lineBottom}
+      $lineColor={$lineColor}
+      size={size}
+    >
       {children}
     </S.Wrapper>
   );
