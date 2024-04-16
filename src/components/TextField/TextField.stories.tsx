34e8fba35f90ecf6dc885047fd77b77db851fdb1
Thiago Kachinsky <thiagokachidev@gmail.com>
2024-04-16 20:48:05 -0300

    feat: create radio and textfield input

---
+++ src/components/TextField/TextField.stories.tsx
@@ -0,0 +1,36 @@
+import { Meta, StoryFn } from '@storybook/react';
+import { Email } from '@styled-icons/material-outlined';
+import { TextField, TextFieldProps } from '.';
+
+export default {
+  title: 'Form/TextField',
+  component: TextField,
+  args: {
+    label: 'E-mail',
+    labelFor: 'Email',
+    icon: <Email />,
+    id: 'Email',
+    initialValue: '',
+    placeholder: 'john.cage@gmail.com',
+  },
+  argTypes: {
+    onInput: { action: 'changed' },
+    icon: { type: 'symbol' },
+  },
+} as Meta;
+
+export const Default: StoryFn<TextFieldProps> = (args) => (
+  <div style={{ maxWidth: 300, padding: 15 }}>
+    <TextField {...args} />
+  </div>
+);
+
+export const withError: StoryFn<TextFieldProps> = (args) => (
+  <div style={{ maxWidth: 300, padding: 15 }}>
+    <TextField {...args} />
+  </div>
+);
+
+withError.args = {
+  error: 'Ops...something is wrong',
+};
